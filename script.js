cdf62b39ee253a477b72a9ab57d828b7a03c4fd8
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-27 14:11:16 +0200

    Add initial HTML and JavaScript files for train ticket pricing application

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km(0.21 € al km)
+- va applicato uno sconto del 20 % per i minorenni
+- va applicato uno sconto del 40 % per gli over 65.
+*/
+
+/* TIP: per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
+100km, 10 anni => prezzo corretto: €16.80
+100km, 70 anni => prezzo corretto: €12.60
+*/
+
+
+// Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+
+
+
+
+
+
+
+// Calcolare il prezzo totale del viaggio, secondo le regole definite in consegna (output = in forma umana - con massimo due decimali, per indicare centesimi sul prezzo).
